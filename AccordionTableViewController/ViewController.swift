31c051822055b35d047608ea79588d6fe091c4ea
Giovanni Lodi <giovanni.lodi42@gmail.com>
2019-07-17 07:51:53 +1000

    New single view application via template project

---
+++ AccordionTableViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccordionTableViewController
+//
+//  Created by Giovanni on 17/7/19.
+//  Copyright © 2019 iflix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
