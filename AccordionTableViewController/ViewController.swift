b48e7316b574425a53c4de876854e64ba9bdc5e2
Giovanni Lodi <giovanni.lodi42@gmail.com>
2019-07-17 07:52:51 +1000

    Get rid of Xcode's cruft

--- AccordionTableViewController/ViewController.swift
+++ AccordionTableViewController/ViewController.swift
@@ -1,20 +1,4 @@
-//
-//  ViewController.swift
-//  AccordionTableViewController
-//
-//  Created by Giovanni on 17/7/19.
-//  Copyright © 2019 iflix. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
