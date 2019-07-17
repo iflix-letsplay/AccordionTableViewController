b103ca9f7dd90c031b9b1213023b7820a8994922
Giovanni Lodi <giovanni.lodi42@gmail.com>
2019-07-17 10:58:16 +1000

    Add table view

--- AccordionTableViewController/AccordionTableViewController.swift
+++ AccordionTableViewController/AccordionTableViewController.swift
@@ -2,8 +2,60 @@ import UIKit
 
 class AccordionTableViewController: UIViewController {
 
+    let tableView = UITableView(frame: .zero)
+
+    let items = ["Pizza", "Pasta", "Curry"]
+
+    let cellIdentifier = "cell"
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .red
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        [
+            NSLayoutConstraint.Attribute.top,
+            NSLayoutConstraint.Attribute.right,
+            NSLayoutConstraint.Attribute.bottom,
+            NSLayoutConstraint.Attribute.left,
+        ]
+            .forEach { attribute in
+                view.addConstraint(
+                    NSLayoutConstraint(
+                        item: tableView,
+                        attribute: attribute,
+                        relatedBy: .equal,
+                        toItem: view,
+                        attribute: attribute,
+                        multiplier: 1,
+                        constant: 0
+                    )
+                )
+            }
+    }
+}
+
+extension AccordionTableViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+        guard indexPath.row < items.count else { return cell }
+
+        let item = items[indexPath.row]
+
+        cell.textLabel?.text = item
+
+        return cell
     }
 }
