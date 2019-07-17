efcf765978bdd472e1522030503dbc96edbd1517
Giovanni Lodi <giovanni.lodi42@gmail.com>
2019-07-17 16:15:07 +1000

    Migrate to a dedicated object for the data structure
    The use of generics required me to remove the delegate and move to a
    closure injection approach.

--- AccordionTableViewController/AccordionTableViewController.swift
+++ AccordionTableViewController/AccordionTableViewController.swift
@@ -1,80 +1,77 @@
 import UIKit
 
-protocol AccordionTableViewControllerDelegate: class {
+// Using `CustomStringConvertible` so that we can easily set the text of the cell.
+// Later on we'll define some kind of closure to select and configure a given cell based on an
+// item `T`.
+class AccordionTableViewController<T: Equatable & CustomStringConvertible>: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    func tableViewController(
-        _ tableViewController: AccordionTableViewController,
-        didSelect item: AccordionTableViewController.Item
-    )
-}
+    struct Banana {
 
-class AccordionTableViewController: UIViewController {
+        struct Node {
+            let item: T
+            let children: [Node]
+            let open: Bool
+        }
 
-    struct Item: Equatable {
-        let text: String
-        let children: [Item]
-        let open: Bool
-    }
+        struct DisplayableNode {
+            let node: Node
+            let depth: Int
+            let parent: Node?
+        }
 
-    struct DisplayItem: Equatable {
-        let item: Item
-        let parent: Item?
+        // TODO: better name?
+        let items: [Node]
 
-        var text: String { return item.text }
+        /// A flattened representation of the items to make displaying in a table view easier
+        var itemsToDisplay: [DisplayableNode] {
+            return items.flatMap { flatten(item: $0) }
+        }
+
+        // TODO: flatten is the wrong name for this
+        private func flatten(
+            item: Node,
+            depth: Int = 0,
+            parent: Node? = .none
+        ) -> [DisplayableNode] {
+            guard item.children.isEmpty == false else {
+                return [DisplayableNode(node: item, depth: depth, parent: parent)]
+            }
+
+            guard item.open else {
+                return [DisplayableNode(node: item, depth: depth, parent: parent)]
+            }
+
+            return [DisplayableNode(node: item, depth: depth, parent: parent)]
+                + item.children.flatMap { flatten(item: $0, depth: depth + 1, parent: item) }
+        }
+
+        func item(for indexPath: IndexPath) -> DisplayableNode? {
+            guard indexPath.row < itemsToDisplay.count else { return .none }
+            return itemsToDisplay[indexPath.row]
+        }
     }
 
     let tableView = UITableView(frame: .zero)
 
-    var items = [
-        Item(
-            text: "Pizza",
-            children: [
-                Item(text: "Margherita", children: [], open: false),
-                Item(text: "Pepperoni", children: [], open: false)
-            ],
-            open: true
-        ),
-        Item(
-            text: "Pasta",
-            children: [],
-            open: true
-        ),
-        Item(
-            text: "Curry",
-            children: [
-                Item(text: "Mild", children: [], open: false),
-                Item(text: "Spicy", children: [], open: false)
-            ],
-            open: false
-        ),
-    ]
-
-
-    var displayItems: [DisplayItem] {
-        return items.flatMap { flatten(item: $0) }
-    }
-
-    weak var delegate: AccordionTableViewControllerDelegate?
-
-    // TODO: flatten is the wrong name for this
-    private func flatten(item: Item, parent: Item? = .none) -> [DisplayItem] {
-        guard item.children.isEmpty == false else {
-            return [DisplayItem(item: item, parent: .none)]
-        }
-
-        guard item.open else {
-            return [DisplayItem(item: item, parent: .none)]
-        }
-
-        return [DisplayItem(item: item, parent: parent)] + item.children.flatMap { flatten(item: $0, parent: item) }
-    }
+    var banana: Banana
+    // Defined as configurable after init so that we can refer to `self` in it and do things like
+    // presenting alerts from it.
+    // Not sure if that's a good approach, will have to see...
+    var onSelect: ((T) -> Void)?
 
     let cellIdentifier = "cell"
 
+    init(banana: Banana) {
+        self.banana = banana
+        super.init(nibName: .none, bundle: .none)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        delegate = self
 
         view.backgroundColor = .red
 
@@ -105,96 +102,80 @@ class AccordionTableViewController: UIViewController {
                 )
             }
     }
-
-    func item(for indexPath: IndexPath) -> DisplayItem? {
-        guard indexPath.row < displayItems.count else { return .none }
-        return displayItems[indexPath.row]
-    }
-}
-
-extension AccordionTableViewController: UITableViewDataSource {
+//}
+//
+//extension AccordionTableViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return displayItems.count
+        return banana.itemsToDisplay.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
 
-        guard let item = item(for: indexPath) else { return cell }
+        guard let item = banana.item(for: indexPath) else { return cell }
 
-        cell.textLabel?.text = item.text
+        cell.textLabel?.text = item.node.item.description
 
         return cell
     }
-}
-
-extension AccordionTableViewController: UITableViewDelegate {
+//}
+//
+//extension AccordionTableViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
 
-        guard let displayItem = item(for: indexPath) else { return }
+        guard let displayItem = banana.item(for: indexPath) else { return }
 
-        if let _ = displayItem.parent {
-            // TODO:
-        } else {
-            // if there's no parent then we have a root item to expand/collapse
-            guard displayItem.item.children.isEmpty == false else {
-                delegate?.tableViewController(self, didSelect: displayItem.item)
-                return
-            }
-
-            guard let index = items.firstIndex(where: { $0.text == displayItem.item.text }) else { return }
-            guard let displayIndex = displayItems.firstIndex(where: { $0.text == displayItem.item.text }) else { return }
-
-            let newItem = Item(
-                text: displayItem.item.text,
-                children: displayItem.item.children,
-                open: !displayItem.item.open // TODO: add `flipped` Bool extension
-            )
-
-            var newItems = items
-            newItems.remove(at: index)
-            newItems.insert(newItem, at: index)
-
-            let indexes = ((displayIndex + 1)..<(displayIndex + 1 + displayItem.item.children.count))
-
-            if displayItem.item.open {
-                tableView.performBatchUpdates(
-                    {
-                        indexes.forEach { rowIndex in
-                            tableView.deleteRows(at: [IndexPath(row: rowIndex, section: 0)], with: .top)
-                        }
-                        items = newItems
-                },
-                    completion: .none
-                )
-            } else {
-                tableView.performBatchUpdates(
-                    {
-                        indexes.forEach { rowIndex in
-                            tableView.insertRows(at: [IndexPath(row: rowIndex, section: 0)], with: .top)
-                        }
-                        items = newItems
-                },
-                    completion: .none
-                )
-            }
+        guard displayItem.parent == nil else {
+            // while we have a soft rule for the depth of the tree being 2 we can assume that
+            // if the item has a parent then it's not one that should be expanded/collapsed
+            onSelect?(displayItem.node.item)
+            return
         }
-    }
-}
 
-extension AccordionTableViewController: AccordionTableViewControllerDelegate {
+        // if there's no parent then we have a root item to expand/collapse
+        guard displayItem.node.children.isEmpty == false else {
+            onSelect?(displayItem.node.item)
+            return
+        }
 
-    func tableViewController(_ tableViewController: AccordionTableViewController, didSelect item: AccordionTableViewController.Item) {
-        let alert = UIAlertController(
-            title: .none,
-            message: item.text,
-            preferredStyle: .alert
+        guard let index = banana.items.firstIndex(where: { $0.item == displayItem.node.item }) else { return }
+        guard let displayIndex = banana.itemsToDisplay.firstIndex(where: { $0.node.item == displayItem.node.item }) else { return }
+
+        let newItem = Banana.Node(
+            item: displayItem.node.item,
+            children: displayItem.node.children,
+            open: !displayItem.node.open // TODO: add `flipped` Bool extension
         )
-        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: .none))
 
-        present(alert, animated: true, completion: .none)
+        var newItems = banana.items
+        newItems.remove(at: index)
+        newItems.insert(newItem, at: index)
+
+        let indexes = ((displayIndex + 1)..<(displayIndex + 1 + displayItem.node.children.count))
+
+        if displayItem.node.open {
+            tableView.performBatchUpdates(
+                {
+                    indexes.forEach { rowIndex in
+                        tableView.deleteRows(at: [IndexPath(row: rowIndex, section: 0)], with: .top)
+                    }
+                    banana = Banana(items: newItems)
+            },
+                completion: .none
+            )
+        } else {
+            tableView.performBatchUpdates(
+                {
+                    indexes.forEach { rowIndex in
+                        tableView.insertRows(at: [IndexPath(row: rowIndex, section: 0)], with: .top)
+                    }
+                    banana = Banana(items: newItems)
+            },
+                completion: .none
+            )
+        }
     }
 }
--- AccordionTableViewController/AppDelegate.swift
+++ AccordionTableViewController/AppDelegate.swift
@@ -7,7 +7,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = AccordionTableViewController()
+        let vc = AccordionTableViewController<String>(
+            banana: AccordionTableViewController.Banana(
+                items: [
+                    AccordionTableViewController.Banana.Node(
+                        item: "Pizza",
+                        children: [
+                            AccordionTableViewController.Banana.Node(
+                                item: "Margherita", children: [], open: false
+                            ),
+                            AccordionTableViewController.Banana.Node(
+                                item: "Pepperoni", children: [], open: false
+                            )
+                        ],
+                        open: true
+                    ),
+                    AccordionTableViewController.Banana.Node(
+                        item: "Pasta", children: [], open: false
+                    ),
+                    AccordionTableViewController.Banana.Node(
+                        item: "Curry",
+                        children: [
+                            AccordionTableViewController.Banana.Node(
+                                item: "Mild", children: [], open: false
+                            ),
+                            AccordionTableViewController.Banana.Node(
+                                item: "Spicy", children: [], open: false
+                            )
+                        ],
+                        open: false
+                    ),
+                ]
+            )
+        )
+
+        vc.onSelect = { item in
+            let alert = UIAlertController(
+                title: .none,
+                message: item,
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: .none))
+
+            vc.present(alert, animated: true, completion: .none)
+        }
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
         return true
     }
